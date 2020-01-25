3b251a4d502387f0980a0a9d0edb879cb30e1916
jose gregorio hernandez calderon <systemgregoryp.c@gmail.com>
2020-01-25 03:53:02 -0400

    Create ND_Ports.hpp

---
+++ include/ND/ND_Ports.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file ND_Ports.hpp
+ * @brief Functions to input/output hardware ports
+ * @author jose gregorio hernandez & agatha IA
+ * */
+#ifndef ND_PORTS_HPP
+#define ND_PORTS_HPP
+
+#include <ND_Types.hpp>
+
+namespace ND{
+	namespace Ports{
+			uint8_t InputB(uint16_t _port);
+			uint16_t InputW(uint16_t port);
+			void OutputB(uint16_t port, uint8_t value); 
+		}
+	}
+#endif
