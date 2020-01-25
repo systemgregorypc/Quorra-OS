7896e6824ec231bacfd0c01fed86290e39b70e8f
jose gregorio hernandez calderon <systemgregoryp.c@gmail.com>
2020-01-25 02:50:43 -0400

    Create ND_Keyboard.cpp

---
+++ src/start/ND_Keyboard.cpp
@@ -0,0 +1,67 @@
+/**
+ * @file ND_Keyboard.cpp
+ * @author jose gregorio hc & agatha IA
+ * @brief The keyboard handler
+ * */
+
+#include <ND_Keyboard.hpp>
+#include <ND_Keymap.hpp>
+#include <ND_Ports.hpp>
+#include <ND_Screen.hpp>
+#include <ND_IRQ.hpp>
+
+#define MAX_STRING_SIZE 1024
+
+unsigned int tmp;
+char stringBuffer[MAX_STRING_SIZE];
+unsigned int stringPos=0;
+
+void ND::Keyboard::Setup()
+{
+	ND::Screen::SetColor(ND_SIDE_FOREGROUND, ND_COLOR_BLACK);
+	ND::Screen::PutString("\nSetup Keyboard...");
+
+	ND_IRQ_InstallHandler(1,&ND_Keyboard_Handler);
+
+	ND::Screen::SetColor(ND_SIDE_FOREGROUND,ND_COLOR_GREEN);
+	ND::Screen::PutString("done");
+	tmp = 1;
+}
+void ND::Keyboard::wait()
+{
+
+}
+unsigned char ND::Keyboard::GetChar()
+{
+	unsigned char scancode;
+	scancode=(unsigned char)ND::Ports::InputB(0x60);
+		
+	if(scancode & ND_KEYBOARD_KEY_RELEASE)
+	{
+		return 255;
+	}else{
+		return en_US[scancode];
+	}
+}
+char* ND::Keyboard::GetString()
+{
+	while(stringBuffer[stringPos-1]!='\n')
+	{
+		
+	}
+	stringPos=0;
+	
+	return stringBuffer;
+}
+
+extern "C"
+void ND_Keyboard_Handler(struct regs* r)
+{
+    unsigned char scancode = ND::Keyboard::GetChar();
+    if(scancode!=255)
+    {
+		ND::Screen::PutChar(scancode);
+		stringBuffer[stringPos]=scancode;
+		stringPos++;
+	}
+}
