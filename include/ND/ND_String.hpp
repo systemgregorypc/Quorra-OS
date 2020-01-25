acc390d038eaa0fa4abadc7aa6aa185c9b9518d7
jose gregorio hernandez calderon <systemgregoryp.c@gmail.com>
2020-01-25 03:50:31 -0400

    Create ND_String.hpp

---
+++ include/ND/ND_String.hpp
@@ -0,0 +1,26 @@
+/**
+ * @file ND_String.hpp
+ * @author jose gregorio hernandez & agatha ia
+ * @brief Functions for manipulate strings
+ * */
+
+#ifndef ND_STRING_HPP
+#define ND_STRING_HPP
+
+#include <ND_Types.hpp>
+
+namespace ND{
+		namespace String{
+				size_t Length(const char* src);
+				int Copy(char* dest, const char* src);
+				int Compare(const char *p1, const char *p2);
+				char* Concatenate(char *dest, const char *src);
+				bool IsUpper(char c);
+				bool IsLower(char c);
+				bool IsDigit(char c);
+				char* itoa(int num);
+				int NumberLength(int num);
+			}
+	
+	}
+#endif
